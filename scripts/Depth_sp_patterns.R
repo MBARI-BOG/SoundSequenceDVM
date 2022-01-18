6e1489f06a383c3cf312d30c05e41530f29d7029
Kathleen Pitz <kpitz@users.noreply.github.com>
2022-01-18 15:25:47 -0800

    try to plot top taxa by cast

--- scripts/Depth_sp_patterns.R
+++ scripts/Depth_sp_patterns.R
@@ -111,8 +111,281 @@ out <- limit_by_meta(meta_filt, taxa_new, potu_filt)
 potu_filt <- out[[1]]
 taxa_filt <- out[[2]]
 
+# Need to limit samples with <500 reads
+
+samp_500 <- potu_filt %>%
+  group_by(SampleID) %>%
+  mutate(total_reads = sum(reads))%>%
+  ungroup() %>%
+  distinct(SampleID,total_reads, .keep_all=FALSE) %>%
+  filter(total_reads >=100)
+
+potu_filt <- left_join(samp_500, potu_filt)
+meta_filt <- left_join(samp_500, meta_filt)
 
 # Plot --------------------------------------------------------------------
+
+#look at number of reads without hake and anchovy:
+test <- potu_filt %>%
+  group_by(SampleID) %>%
+  mutate(total_reads = sum(reads))%>%
+  ungroup() %>%
+  distinct(SampleID, .keep_all=TRUE)
+
+# Just MARS Casts, through time, showing depth 'center' of abundance?
+test <- inner_join(potu_filt, meta_filt %>% select(SampleID, SAMPLING_station,SAMPLING_station_number,depth, local_time, time_label,diel, PlateID),  by = c("SampleID")) %>%
+  mutate(time = mdy_hm(local_time)) %>%
+  mutate(time_since = as.numeric(time)) %>%
+  mutate(ESP = case_when(str_detect(SampleID, 'SC')==TRUE ~'ESP',
+                         str_detect(SampleID, 'Bongo')==TRUE ~'Bongo',
+                         TRUE~'CTD')) %>%
+  inner_join(species_label,  by = c("ASV")) %>%
+  #filter(Genus == 'Stenobrachius') %>%
+  filter(Genus == 'Lipolagus') %>%
+  #filter(Genus == 'Engraulis') %>%
+  #filter(reads>1) %>%
+  #mutate(ASVs=1) %>%
+  group_by(Genus, SampleID) %>%
+  mutate(per_tot = sum(per_tot)) %>%
+  mutate(reads = sum(reads)) %>%
+  #mutate(ASVs= sum(ASVs)) %>%
+  ungroup() %>%
+  distinct(Genus, SampleID, .keep_all=TRUE) %>%
+  filter(ESP!='Bongo') %>%
+  filter(SAMPLING_station_number >=1) %>%
+  filter(SAMPLING_station=='MARS') %>%
+  arrange(SAMPLING_station_number) %>%
+  mutate(SAMPLING_station_number = replace(SAMPLING_station_number, SAMPLING_station_number=='3', '03')) %>%
+  mutate(local_time = mdy_hm(local_time))
+
+center <- test %>%
+  #group_by(SampleID) %>%
+  mutate(value = depth*per_tot) %>%
+  mutate(count=1) %>%
+  #ungroup() %>%
+  group_by(SAMPLING_station_number) %>%
+  mutate(sum_value = sum(value)) %>%
+  #mutate(count_value = sum(count)) %>%
+  mutate(count_value = sum(per_tot)) %>%
+  mutate(mean_depth2 = sum_value/count_value) %>%
+  #mutate(mean_depth = mean(value)) %>%
+  distinct(SAMPLING_station_number, .keep_all=TRUE) %>%
+  select(mean_depth2, local_time)
+
+
+test %>%
+  left_join(center) %>%
+  filter(SAMPLING_station_number %in% c('11', '13', '16', '20', '23', '27') ==FALSE) %>%
+  #unite(label, SAMPLING_station_number,local_time, SAMPLING_station,sep='_') %>%
+  ggplot(aes(y=depth, x=local_time))+
+  #geom_bar(stat='identity')+
+  geom_point(aes(size=per_tot, color=diel))+
+  #geom_point(aes(y=mean_depth2,size=1),color='black')+
+  geom_line(aes(y=mean_depth2), color='black', size=1, linetype = "dashed") +
+  #geom_line(data = center, aes(x=local_time, y=mean_depth2)) %>%
+  #coord_flip()+
+  scale_y_reverse()
+
+filename = paste(directory, marker,'_Lipolagus_casts_throughtime_nohake_noanch.png', sep='')
+print(var)
+filename
+ggsave(filename,height = 5, width =10, units = 'in')
+
+#Try to plot bars instead of points through time.
+#By cast?
+
+test %>%
+  left_join(center) %>%
+  filter(SAMPLING_station_number %in% c('11', '13', '16', '20', '23', '27') ==FALSE) %>%
+  #unite(label, SAMPLING_station_number,local_time, SAMPLING_station,sep='_') %>%
+  ggplot(aes(x=depth, y=per_tot))+
+  geom_bar(aes( y=100),stat='identity', fill = "grey",alpha=0.8, width=10)+
+  geom_bar(stat='identity', aes(fill = diel), width=10)+
+  coord_flip()+
+  scale_x_reverse()+
+  facet_wrap(~ SAMPLING_station_number)+
+  labs(x='depth (m)', y='Percent Total Reads', title="Lipolagus by Cast") +
+  #day night transition
+  scale_fill_manual(values = c("darkgoldenrod1", "deepskyblue", "chartreuse2"))
+
+filename = paste(directory, marker,'_Lipolagus_bycast_nohake_noanch.png', sep='')
+print(var)
+filename
+ggsave(filename,height = 8, width =10, units = 'in')
+
+
+
+# Stacked Bar by Cast -----------------------------------------------------
+
+taxas <- c('Class','Order', 'Family', 'Genus', 'Species')
+#### Percent Reads
+for (val in taxas) {
+  taxa_level = sym(val)
+  top_taxa <- potu_filt %>%
+    full_join(species_label) %>%
+    # filter(!!taxa_level != 'Unknown') %>%
+    # filter(!!taxa_level !='no_hit') %>%
+    filter(!!taxa_level !='unassigned') %>%
+    # filter(!!taxa_level !='unknown') %>%
+    # filter(!!taxa_level !='s_') %>%
+    # filter(!!taxa_level !='g_') %>%
+    group_by(!!taxa_level) %>%
+    mutate(sum_per_tot = sum(per_tot)) %>%
+    distinct(!!taxa_level,.keep_all = TRUE ) %>%
+    arrange(-sum_per_tot) %>%
+    select(Kingdom, Phylum, Class, Order, Family,Genus, Species, sum_per_tot) %>%
+    #print(n = Inf) %>%
+    ungroup() %>%
+    select(!!taxa_level, sum_per_tot) %>%
+    top_n(10)
+  
+  # assign text colour
+  textcol <- "grey40"
+  print("Begin plotting...")
+  bp_top <- inner_join(potu_filt, meta_filt,  by = c("SampleID")) %>% #join with metadata
+    inner_join(species_label,  by = c("ASV")) %>%  #join with taxonomy
+    right_join(top_taxa) %>% #limit to top taxa
+    filter(SAMPLING_station_number >=1) %>%
+    filter(SAMPLING_station=='MARS') %>%
+    #arrange(SAMPLING_station_number) %>%
+    mutate(SAMPLING_station_number = replace(SAMPLING_station_number, SAMPLING_station_number=='3', '03')) %>%
+    filter(SAMPLING_station_number %in% c('11', '13', '16', '20', '23', '27') ==FALSE) %>%
+    ggplot(aes(x=depth, y=per_tot))+
+    geom_bar(aes( y=1),stat='identity', fill = "grey",alpha=0.4, width=10)+
+    geom_bar(stat='identity', aes(fill = !!taxa_level), width=10)+
+    coord_flip()+
+    scale_x_reverse()+
+    facet_wrap(~ SAMPLING_station_number)+
+    scale_fill_tableau(palette = "Tableau 20", type = c("regular"), direction = 1)+
+    labs(x="",y="Percent Total Reads")+
+    theme_minimal() +
+    guides(fill=guide_legend(ncol=2)) +
+    theme(
+      #legend
+      legend.position="bottom",legend.direction="vertical",
+      legend.text=element_text(colour=textcol,size=8,face="bold"),
+      legend.key.height=grid::unit(0.3,"cm"),
+      legend.key.width=grid::unit(0.3,"cm"),
+      legend.title=element_text(colour=textcol,size=8,face="bold"),
+      axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1,size=5,colour=textcol),
+      #axis.text.x=element_text(size=7,colour=textcol),
+      axis.text.y=element_text(size=6,colour=textcol),
+      axis.title.y = element_text(size=6),
+      plot.background=element_blank(),
+      panel.border=element_blank(),
+      panel.grid.minor = element_blank(),
+      panel.grid.major = element_line(size = .25),
+      plot.margin=margin(0.1,0.1,0.1,0.1,"cm"),
+      plot.title=element_blank())
+  
+  filename = paste(directory, marker,'_top10',taxa_level,'_bar_bycast.png', sep='')
+  #print('Plot of top 20 Genus average by month:')
+  print(filename)
+  ggsave(filename,height = 8, width =10, units = 'in')
+}
+
+
+test <- inner_join(potu_filt, meta_filt %>% select(SampleID, SAMPLING_station,SAMPLING_station_number,depth, local_time, time_label,diel, PlateID),  by = c("SampleID")) %>%
+  mutate(time = mdy_hm(local_time)) %>%
+  mutate(time_since = as.numeric(time)) %>%
+  mutate(ESP = case_when(str_detect(SampleID, 'SC')==TRUE ~'ESP',
+                         str_detect(SampleID, 'Bongo')==TRUE ~'Bongo',
+                         TRUE~'CTD')) %>%
+  inner_join(species_label,  by = c("ASV")) %>%
+  #filter(Genus == 'Stenobrachius') %>%
+  filter(Genus == 'Lipolagus') %>%
+  #filter(Genus == 'Engraulis') %>%
+  #filter(reads>1) %>%
+  #mutate(ASVs=1) %>%
+  group_by(Genus, SampleID) %>%
+  mutate(per_tot = sum(per_tot)) %>%
+  mutate(reads = sum(reads)) %>%
+  #mutate(ASVs= sum(ASVs)) %>%
+  ungroup() %>%
+  distinct(Genus, SampleID, .keep_all=TRUE) %>%
+  filter(ESP!='Bongo') %>%
+  filter(SAMPLING_station_number >=1) %>%
+  filter(SAMPLING_station=='MARS') %>%
+  arrange(SAMPLING_station_number) %>%
+  mutate(SAMPLING_station_number = replace(SAMPLING_station_number, SAMPLING_station_number=='3', '03')) %>%
+  mutate(local_time = mdy_hm(local_time))
+
+# All Samples -------------------------------------------------------------
+
+
+
+# All samples
+
+test <- inner_join(potu_filt, meta_filt %>% select(SampleID, SAMPLING_station,SAMPLING_station_number,depth, local_time, time_label,diel, PlateID),  by = c("SampleID")) %>%
+  mutate(time = mdy_hm(local_time)) %>%
+  mutate(time_since = as.numeric(time)) %>%
+  mutate(ESP = case_when(str_detect(SampleID, 'SC')==TRUE ~'ESP',
+                         str_detect(SampleID, 'Bongo')==TRUE ~'Bongo',
+                         TRUE~'CTD')) %>%
+  inner_join(species_label,  by = c("ASV")) %>%
+  #filter(Genus == 'Stenobrachius') %>%
+  filter(Genus == 'Leuroglossus') %>%
+  group_by(Genus, SampleID) %>%
+  mutate(per_tot = sum(per_tot)) %>%
+  mutate(reads = sum(reads)) %>%
+  ungroup() %>%
+  distinct(Genus, SampleID, .keep_all=TRUE) %>%
+  filter(ESP!='Bongo') %>%
+  mutate(local_time = mdy_hm(local_time))
+
+# center <- test %>%
+#   #group_by(SampleID) %>%
+#   mutate(value = depth*per_tot) %>%
+#   mutate(count=1) %>%
+#   #ungroup() %>%
+#   group_by(SAMPLING_station_number) %>%
+#   mutate(sum_value = sum(value)) %>%
+#   #mutate(count_value = sum(count)) %>%
+#   mutate(count_value = sum(per_tot)) %>%
+#   mutate(mean_depth2 = sum_value/count_value) %>%
+#   #mutate(mean_depth = mean(value)) %>%
+#   distinct(SAMPLING_station_number, .keep_all=TRUE) %>%
+#   select(mean_depth2, local_time)
+
+
+test %>%
+  #left_join(center) %>%
+  #filter(ESP=='ESP') %>%
+  filter(SAMPLING_station_number %in% c('11', '13', '16', '20', '23', '27') ==FALSE) %>%
+  #unite(label, SAMPLING_station_number,local_time, SAMPLING_station,sep='_') %>%
+  ggplot(aes(y=depth, x=local_time))+
+  #geom_bar(stat='identity')+
+  geom_point(aes(size=reads, color=ESP), alpha=0.8)+
+  #geom_point(aes(y=mean_depth2,size=1),color='black')+
+  #geom_line(aes(y=mean_depth2), color='black', size=1, linetype = "dashed") +
+  #geom_line(data = center, aes(x=local_time, y=mean_depth2)) %>%
+  #coord_flip()+
+  scale_y_reverse() +
+  scale_color_manual(values = c("darkgoldenrod1", "deepskyblue", "chartreuse2"))
+
+filename = paste(directory, marker,'_Leuroglossus_casts_throughtime_nohake_noanch_reads.png', sep='')
+print(var)
+filename
+ggsave(filename,height = 5, width =10, units = 'in')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Stenobrachius
 test <- inner_join(potu_filt, 
